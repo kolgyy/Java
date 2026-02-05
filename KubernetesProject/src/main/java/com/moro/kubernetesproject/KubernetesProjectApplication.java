29acd990345e6ab99bce297535f9743b0e5885a8
Edward Korotaev <eduardkorotaev77@gmail.com>
2026-02-05 15:53:12 +0200

    Added project

---
+++ KubernetesProject/src/main/java/com/moro/kubernetesproject/KubernetesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.moro.kubernetesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesProjectApplication.class, args);
+    }
+
+}
---
+++ KubernetesProject/src/test/java/com/moro/kubernetesproject/KubernetesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moro.kubernetesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
