33ea170d3741efd40373fccaaa922447261a9b59
Edward Korotaev <eduardkorotaev77@gmail.com>
2026-02-20 04:43:37 +0200

    feat(bot): add setupTelegramCommands bean in TelegramBotInitializer
    - Automatically sets commands in Telegram client

--- bot/src/main/java/backend/academy/linktracker/bot/configuration/TelegramBotInitializer.java
+++ bot/src/main/java/backend/academy/linktracker/bot/configuration/TelegramBotInitializer.java
@@ -3,9 +3,12 @@ package backend.academy.linktracker.bot.configuration;
 import backend.academy.linktracker.bot.service.TelegramCommandService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
+import com.pengrad.telegrambot.request.SetMyCommands;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,5 +26,14 @@ public class TelegramBotInitializer {
 
         telegramBot.setUpdatesListener(listener);
         return listener;
+    }
+
+    @Bean
+    public void setupTelegramCommands() {
+        List<BotCommand> botCommands = commandService.getCommandRegistry().getCommands().stream()
+            .map(cmd -> new BotCommand(cmd.name(), cmd.description()))
+            .toList();
+
+        telegramBot.execute(new SetMyCommands(botCommands.toArray(new BotCommand[0])));
     }
 }
