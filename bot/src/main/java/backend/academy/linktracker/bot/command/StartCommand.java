2cd044258213b561d0e90802495b213487ab9700
Edward Korotaev <eduardkorotaev77@gmail.com>
2026-02-20 03:30:40 +0200

    refactor(bot): make StartCommand implement UserCommand

--- bot/src/main/java/backend/academy/linktracker/bot/command/StartCommand.java
+++ bot/src/main/java/backend/academy/linktracker/bot/command/StartCommand.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class StartCommand implements Command {
+public class StartCommand implements UserCommand {
 
     private final UserService userService;
 
