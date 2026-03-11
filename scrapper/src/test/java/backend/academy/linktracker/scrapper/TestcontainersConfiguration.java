1305287dd97634a88fe6a3536887280dd79389db
Edward Korotaev <eduardkorotaev77@gmail.com>
2026-03-11 21:49:09 +0200

    test(scrapper): delete TestcontainersConfiguration.java

--- scrapper/src/test/java/backend/academy/linktracker/scrapper/TestcontainersConfiguration.java
+++
@@ -1,9 +0,0 @@
-package backend.academy.linktracker.scrapper;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-    // Add your container definitions here
-}
