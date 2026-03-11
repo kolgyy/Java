1fc1a0955c76517c3579b3ea3d39a0e27e952faa
Edward Korotaev <eduardkorotaev77@gmail.com>
2026-03-11 21:25:38 +0200

    feat(scrapper): add StackoverflowTrackedLink

---
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/model/StackoverflowTrackedLink.java
@@ -0,0 +1,28 @@
+package backend.academy.linktracker.scrapper.model;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@RequiredArgsConstructor
+public class StackoverflowTrackedLink implements TrackedLink {
+
+    private final Link link;
+    private final String questionId;
+
+    @Setter
+    private OffsetDateTime lastUpdated;
+
+    @Override
+    public Optional<OffsetDateTime> getLastUpdated() {
+        return Optional.ofNullable(lastUpdated);
+    }
+
+    @Override
+    public LinkType getType() {
+        return LinkType.STACKOVERFLOW;
+    }
+}
