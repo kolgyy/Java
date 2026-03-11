e765d93ef4a0a5d2662456bdf65703170f44bfa6
Edward Korotaev <eduardkorotaev77@gmail.com>
2026-03-11 21:48:17 +0200

    feat(scrapper): update ScrapperApplication for scheduling

--- scrapper/src/main/java/backend/academy/linktracker/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/ScrapperApplication.java
@@ -3,8 +3,10 @@ package backend.academy.linktracker.scrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ConfigurationPropertiesScan
 public class ScrapperApplication {
 
